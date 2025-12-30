0ca3544d815ad099acbc3ecfede12b17c10e2c21
SomeCodecat <88855796+SomeCodecat@users.noreply.github.com>
2025-12-30 07:04:53 +0100

    fix: bind server to 0.0.0.0 to allow external connections

--- server.ts
+++ server.ts
@@ -19,8 +19,8 @@ const logger = pino({
 });
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = 3000;
+const hostname = process.env.HOSTNAME || "0.0.0.0";
+const port = parseInt(process.env.PORT || "3000", 10);
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
